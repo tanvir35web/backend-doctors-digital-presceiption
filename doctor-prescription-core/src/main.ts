743ff9abb0fde6e187b4819c2d8ae75865dca355
tanvir35web <islammdtanvirul@gmail.com>
2026-04-04 13:08:14 +0600

    feat(cors): Enable CORS with specific settings for the application

--- doctor-prescription-core/src/main.ts
+++ doctor-prescription-core/src/main.ts
@@ -4,6 +4,12 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
     new ValidationPipe({
